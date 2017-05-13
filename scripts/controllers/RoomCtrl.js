e22328754192b12e6a1697521952bd3112949cae
Jag <sarai.jagvir@gmail.com>
2017-05-13 10:38:16 -0700

    messages show up when room is selected

--- scripts/controllers/RoomCtrl.js
+++ scripts/controllers/RoomCtrl.js
@@ -1,9 +1,15 @@
 (function(){
-	function RoomCtrl(Room){
+	function RoomCtrl($scope, Room, Message){
 		this.rooms = Room.all;
+		
+		$scope.activeRoom = function(roomValue, roomId){
+			$scope.roomValue = roomValue;
+			$scope.showMessages = Message.getByRoomId(roomId);
+		};	
+		
 	}
 	
 	angular
 		.module('blocChat')
-		.controller('RoomCtrl', ['Room', RoomCtrl]);
+		.controller('RoomCtrl', ['$scope', 'Room', 'Message', RoomCtrl]);
 })();
---
+++ scripts/services/Message.js
@@ -0,0 +1,18 @@
+(function(){
+	function Message($firebaseArray){
+		var Message = {};
+		var ref = firebase.database().ref().child("messages");
+		var messages = $firebaseArray(ref);
+		
+		Message.getByRoomId = function(roomId){
+			var roomMessages = ref.orderByChild("roomId").equalTo(roomId);
+				return $firebaseArray(roomMessages);
+		};
+	
+	return Message;
+		
+	}
+	angular
+		.module('blocChat')
+		.factory('Message', ['$firebaseArray', Message]);
+})();
